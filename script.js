abca1d5b8a89694442864ef2102b8e4f52be12b3
sairoxTV <sairox939@gmail.com>
2024-09-21 00:46:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.getElementById('sidebar');
     const toggleButton = document.getElementById('toggle-button');
 
-    toggleButton.addEventListener('click', function() {
+    // Obsługuje kliknięcie przycisku do przełączania panelu bocznego
+    toggleButton.addEventListener('click', function(event) {
         sidebar.classList.toggle('hidden');
+        event.stopPropagation(); // Zapobiega ukrywaniu menu po kliknięciu przycisku
+    });
+
+    // Ukrywa menu, gdy kliknięto gdzie indziej
+    document.addEventListener('click', function(event) {
+        if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
+            sidebar.classList.add('hidden'); // Ukrywa menu
+        }
     });
 });
