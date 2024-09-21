846a861141867200007017fa84503becacd71dca
sairoxTV <sairox939@gmail.com>
2024-09-21 11:22:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.getElementById('sidebar');
+    const sidebar = document.getElementById('leftPanel');
     const toggleButton = document.getElementById('toggle-button');
 
+    // Funkcja do otwierania/zamykania panelu
     toggleButton.addEventListener('click', function(event) {
-        sidebar.classList.toggle('hidden');
+        sidebar.classList.toggle('active');
         event.stopPropagation(); // Zatrzymuje propagację
     });
 
+    // Zamyka panel, gdy kliknięto poza nim
     document.addEventListener('click', function(event) {
         if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
-            sidebar.classList.add('hidden'); // Ukrywa menu
+            sidebar.classList.remove('active'); // Ukrywa panel
         }
     });
 });
