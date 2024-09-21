93c32631edba04801cab138270adb8e5a271b305
sairoxTV <sairox939@gmail.com>
2024-09-21 12:27:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.getElementById('sidebar');
-    const toggleButton = document.getElementById('toggle-button');
 
-    toggleButton.addEventListener('click', function(event) {
-        sidebar.classList.toggle('hidden'); // Przełączanie klasy 'hidden'
-        event.stopPropagation();
-    });
-
-    document.addEventListener('click', function(event) {
-        if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
-            sidebar.classList.add('hidden'); // Ukrywa panel, jeśli kliknięto poza nim
-        }
-    });
-});
