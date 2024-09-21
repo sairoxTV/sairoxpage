4ad46883c5b1c0e40f165bdcaf63d6adda00c116
sairoxTV <sairox939@gmail.com>
2024-09-21 12:09:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.getElementById('sidebar'); // Używamy identyfikatora 'sidebar'
-    const toggleButton = document.getElementById('toggle-button'); // Przycisk do otwierania/zamykania
+    const sidebar = document.getElementById('sidebar');
+    const toggleButton = document.getElementById('toggle-button');
 
-    // Funkcja do otwierania/zamykania panelu
     toggleButton.addEventListener('click', function(event) {
-        sidebar.classList.toggle('hidden'); // Dodanie/usunięcie klasy 'hidden'
-        event.stopPropagation(); // Zatrzymanie propagacji zdarzenia
+        sidebar.classList.toggle('hidden'); // Przełączanie klasy 'hidden'
+        event.stopPropagation();
     });
 
-    // Zamyka panel, gdy kliknięto poza nim
     document.addEventListener('click', function(event) {
         if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
-            sidebar.classList.add('hidden'); // Ukrywa panel, gdy kliknięto poza nim
+            sidebar.classList.add('hidden'); // Ukrywa panel, jeśli kliknięto poza nim
         }
     });
 });
